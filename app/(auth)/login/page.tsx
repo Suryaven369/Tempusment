79eb0d8f23318682845aed7f7025b179a1c1235b
Suryaven369 <suryavenkatesh369@gmail.com>
2024-12-04 00:08:54 +0530

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "@/lib/firebase";
+//import { auth } from "@/lib/firebase";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
